458f53768155f1d2cb566132ab2a4e40b43397b6
tinubhandari <tinu.mnc7393@gmail.com>
2020-02-23 09:29:12 +0530

    Test1

--- src/test/java/testing/LoginTest.java
+++ src/test/java/testing/LoginTest.java
@@ -1,12 +1,26 @@
 package testing;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class LoginTest {
   @Test
-  public void loginTest() {
+  public void loginTest() throws InterruptedException {
 	  
 	  System.out.println("In testng login test class");
+	  
+	  System.setProperty("webdriver.chrome.driver", "D:\\Java\\SeleniumBeginner\\"
+				+ "chromedriver_win32\\chromedriver.exe");
+	  
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("https://www.facebook.com/");
+	  
+	  Thread.sleep(5000);
+	
+	  
+	  driver.quit();
+	  
 	  
   }
 }
